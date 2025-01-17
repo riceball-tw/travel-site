e963edf45385c681feb126b59c3094c8e3e4777b
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-01-18 01:05:54 +0800

    feat: Add navbar for navgation between home and favorite page

---
+++ components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar">
+    <NuxtLink to="/" class="logo">Travel Site</NuxtLink>
+    <ul class="navbar-list">
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/favorite">Favorite</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss">
+  .navbar {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .navbar-list {
+    display: flex;
+    gap: 1rem;
+  }
+
+  .logo {
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-decoration: none;
+  }
+
+</style>
