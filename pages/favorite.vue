cb4d2361e94cd25227b851ff02dccad170126daa
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-01-18 12:20:48 +0800

    feat: List all favorate attraction in favoriate page

--- pages/favorite.vue
+++ pages/favorite.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    favorite
+  <div class="container">
+    <h1 class="favorite-banner">Favorite Attractions</h1>
+
+    <div v-for="attraction in favoriteAttractions" :key="attraction.id">
+      <button @click="() => toggleFavoriteAttraction(attraction)" type="button">
+        <span v-if="isTargetAttractionExist(favoriteAttractions, attraction.id)">✅</span>
+        <span v-else>😑</span>
+      </button>
+      {{ attraction.name }}
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  const favoriteAttractionStore = useFavoriteAttractionStore()
+  const { favoriteAttractions } = storeToRefs(favoriteAttractionStore)
+  const { toggleFavoriteAttraction, isTargetAttractionExist } = favoriteAttractionStore
+
+</script>
+
+<style lang="scss">
+  .favorite-banner {
+    text-align: center;
+    font-weight: bold;
+    font-size: 3rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+  }
+</style>
